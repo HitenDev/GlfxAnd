f7b590db7e0f82e52975148ba4820036711f381f
lilei <lilei@bbtree.com>
2017-06-21 17:58:43 +0800

    import library

---
+++ app/src/main/java/com/opengles/android/glfx/lib/Slider.java
@@ -0,0 +1,14 @@
+package com.opengles.android.glfx.lib;
+
+/**滑块配置
+ * Created by admin on 2017/6/21.
+ */
+
+public class Slider {
+    public String name;
+    public String label;
+    public Number min;
+    public Number max;
+    public Number value;
+    public Number step;
+}
